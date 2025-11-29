0b3868108b684b9a13dfb8a15cae483d316ef128
Veronica <vlmarrocco@gmail.com>
2025-11-29 10:38:21 -0800

    some final touches

--- src/TestPipelineSpeed.cpp
+++
@@ -1,5 +0,0 @@
-// TODO.
-// See window configs.
-// Timing budget per window for classification pipeline to ensure real time functionality:
-// timing budget = hop (we need to be ready in time for next window which happens after hop)
-// therefore timing budget = 0.85s (need to verify pipeline occurs within 850ms)
--- src/window_configs.hpp
+++ src/window_configs.hpp
@@ -5,6 +5,7 @@
 #include <string>
 
 // central place for sizes used across headers
+// *************** MUST MATCH WINDOW LENGTH IN PYTHON ***********
 inline constexpr std::size_t RING_BUFFER_CAPACITY       = 600;  // ring buffer capacity
 inline constexpr std::size_t WINDOW_SAMPLES    = RING_BUFFER_CAPACITY/3;     // e.g. 1.7 s @ ~119 Hz
 inline constexpr std::size_t WINDOW_HOP        = RING_BUFFER_CAPACITY/6; // 50% overlap
